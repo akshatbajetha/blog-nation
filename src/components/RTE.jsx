da6b8a1351638e29850b2009b7557521525f933d
Akshat Bajetha <akshatbajetha@gmail.com>
2024-08-16 00:43:35 +0530

    update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -12,7 +12,7 @@ export default function RTE({name, control, label, defaultValue = ""}) {
         control={control}
         render={({field: {onChange}}) => (
             <Editor
-            apiKey={config.tinyMceApiKey}
+            apiKey={`${config.tinyMceApiKey}`}
             initialValue={defaultValue}
             init={{
                 initialValue: defaultValue,
