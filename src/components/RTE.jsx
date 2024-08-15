f376153154c8666b5e2f0e623dfd1b25e3422e26
Akshat Bajetha <akshatbajetha@gmail.com>
2024-08-16 00:47:18 +0530

    update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import {Editor} from '@tinymce/tinymce-react'
 import { Controller } from 'react-hook-form'
-import config from '../config/config'
 
 export default function RTE({name, control, label, defaultValue = ""}) {
-  const apiKey = config.tinyMceApiKey
+  const apiKey = import.meta.env.VITE_TINYMCE_API_KEY;
   return (
     <div className='w-full'>
         {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
